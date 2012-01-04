42c9a584d9f1e5aa0a52d19afee241fdf0cc8318
Jun Lin <linjunpop@gmail.com>
2012-01-04 21:40:20 +0800

    change file structure

---
+++ scripts/application.js
@@ -0,0 +1,25 @@
+function newDoc(type) {
+	if (type == 'document') {
+	    url = 'http://docs.google.com/?action=newdoc';
+    }
+	else if (type == 'spreadsheet') {
+	    url = 'http://spreadsheets.google.com/ccc?new';
+    }
+    else if (type == 'presentation') {
+        url = 'http://docs.google.com/?action=new_presentation';
+    }
+    else if (type == 'drawing') {
+        url = 'https://docs.google.com/drawings/create';
+    }
+
+   loadURL(url);
+ }
+
+function loadURL(url) {
+    chrome.tabs.create(
+        {'url': url},
+        function(tab) {
+            // Tab opened.
+        }
+    );
+}
