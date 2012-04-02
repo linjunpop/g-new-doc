ab626221982ddfdad0709446eb8e62ebe86e14f6
Jun Lin <linjunpop@gmail.com>
2012-04-03 07:49:19 +0800

    include zepto.js & clean up code

--- scripts/application.js
+++ scripts/application.js
@@ -1,33 +1,11 @@
-function newDoc(type) {
-  switch(type) {
-  case 'document':
-    url = 'https://docs.google.com/document/d/new';
-    break;
-  case 'presentation':
-    url = 'https://docs.google.com/presentation/create';
-    break;
-  case 'spreadsheet':
-    url = 'http://spreadsheets.google.com/ccc?new';
-    break;
-  case 'form':
-    url = 'https://docs.google.com/spreadsheet/newform';
-    break;
-  case 'drawing':
-    url = 'https://docs.google.com/drawings/create';
-    break;
-  case 'table':
-    url = 'https://www.google.com/fusiontables/DataSource?dsrcid=implicit';
-    break;
-  }
-
-  loadURL(url);
-}
-
-function loadURL(url) {
-  chrome.tabs.create(
-    {'url': url},
-    function(tab) {
-      // Tab opened.
-    }
-  );
-}
+$(function() {
+  $(document).on( 'click', '.link', function() {
+    url = $(this).data('target');
+    chrome.tabs.create(
+      { 'url': url },
+      function(tab) {
+        // Tab opened.
+      }
+    );
+  });
+});
